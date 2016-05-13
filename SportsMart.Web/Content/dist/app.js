ebe072badfb78c47bb71a971adc3125c5d7ceffb
Tailang, Prashant <Prashant.Tailang@mercer.com>
2016-05-13 14:32:34 +0530

    Updated Component Folder

--- SportsMart.Web/Content/dist/app.js
+++ SportsMart.Web/Content/dist/app.js
@@ -9,23 +9,23 @@ angular.module('app', [
     .config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
         // Specify the three simple routes ('/', '/About', and '/Contact')
         $routeProvider.when('/', {
-            templateUrl: 'content/app/components/home/home.html',
+            templateUrl: 'content/app/home/home.html',
             controller: 'home',
         })
             .when('/about', {
-            templateUrl: 'content/app/components/home/about.html',
+            templateUrl: 'content/app/home/about.html',
             controller: 'about',
         })
             .when('/contact', {
-            templateUrl: 'content/app/components/home/contact.html',
+            templateUrl: 'content/app/home/contact.html',
             controller: 'contact'
         })
             .when('/product', {
-            templateUrl: 'content/app/components/product/products.html',
+            templateUrl: 'content/app/product/products.html',
             controller: 'contact'
         })
             .otherwise({
-            templateUrl: 'content/app/components/home/home.html',
+            templateUrl: 'content/app/home/home.html',
             controller: 'home',
         });
         // Specify HTML5 mode (using the History APIs) or HashBang syntax.
